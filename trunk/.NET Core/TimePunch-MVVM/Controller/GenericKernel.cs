e68f605d83a88f227c14ebf022c3d40c0fc5086d
Gerhard Stephan <gerhard.stephan@timepunch.de>
2025-01-05 20:17:28 +0100

    Added a new Generic Kernel

---
+++ trunk/.NET Core/TimePunch-MVVM/Controller/GenericKernel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimePunch.MVVM.EventAggregation;
+
+namespace TimePunch.MVVM.Controller
+{
+    public interface IGenericKernel
+    {
+        /// <summary>
+        /// Gets the event aggregator.
+        /// </summary>
+        /// <returns></returns>
+        IEventAggregator EventAggregator { get; }
+
+        /// <summary>
+        /// Gets the current used based controller
+        /// </summary>
+        IBaseController BaseController { get; }
+    }
+
+    public class GenericKernel : IGenericKernel
+    {
+        /// <summary>
+        /// Gets or sets the Kernel Instance
+        /// </summary>
+        public static IGenericKernel? Instance { get; internal set; }
+
+        /// <summary>
+        /// Gets the event aggregator.
+        /// </summary>
+        /// <returns></returns>
+        public IEventAggregator EventAggregator => Instance?.EventAggregator ?? default!;
+
+        /// <summary>
+        /// Gets the base controller
+        /// </summary>
+        public IBaseController BaseController => Instance?.BaseController ?? default!;
+    }
+}
--- trunk/.NET Core/TimePunch-MVVM/Controller/Kernel.cs
+++ trunk/.NET Core/TimePunch-MVVM/Controller/Kernel.cs
@@ -7,11 +7,20 @@ using TimePunch.MVVM.EventAggregation;
 
 namespace TimePunch.MVVM.Controller
 {
+    public interface IKernel<TProjectController> : IGenericKernel
+        where TProjectController : IBaseController
+    {
+        /// <summary>
+        /// Gets the current used controller
+        /// </summary>
+        /// <returns></returns>
+        TProjectController Controller { get; set; }
+    }
+
     public abstract class Kernel<TProjectKernel, TProjectController> : IKernel<TProjectController>
         where TProjectKernel : IKernel<TProjectController>, new()
         where TProjectController : IBaseController, new()
     {
-
         private static TProjectKernel? singleton;
 
         /// <summary>
@@ -24,8 +33,11 @@ namespace TimePunch.MVVM.Controller
                 if (singleton != null)
                     return singleton;
 
-                singleton = new TProjectKernel();
-                singleton.Controller = new();
+                // ReSharper disable once UseObjectOrCollectionInitializer
+                singleton = new TProjectKernel();   // not used, because of StackOverflowException
+                singleton.Controller = new TProjectController();
+
+                GenericKernel.Instance = singleton;
                 return singleton;
 
             }
@@ -43,5 +55,10 @@ namespace TimePunch.MVVM.Controller
         /// </summary>
         /// <returns></returns>
         public TProjectController Controller { get; set; } = default!;
+
+        /// <summary>
+        /// Gets the current used based controller
+        /// </summary>
+        IBaseController IGenericKernel.BaseController => Controller;
     }
 }
